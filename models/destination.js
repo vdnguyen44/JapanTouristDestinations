ec4908800118656ab056bec52e95ef3d3338c5ff
Vincent Nguyen <vdnguyen44@gmail.com>
2021-02-18 00:21:18 -0800

    Made sure reviews are deleted along with a destination when a destination is deleted.

--- models/destination.js
+++ models/destination.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const DestinationSchema = new Schema({
@@ -14,6 +15,16 @@ const DestinationSchema = new Schema({
         }
     ]
 });
+
+DestinationSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 //Turns schema into model, a model is a class with which we construct documents, every document will be a destination with the specified properties
 module.exports = mongoose.model('Destination', DestinationSchema);
