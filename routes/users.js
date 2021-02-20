8915259aea5db81aeba5de955ab159f349b78aba
Vincent Nguyen <vdnguyen44@gmail.com>
2021-02-20 02:14:11 -0800

    added login/register/logout buttons to navbar based on user login status. If user initiates action that requires login but is not logged in, they are redirected to the page intended after they log in.

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.flash('success', 'Youkoso!');
-        res.redirect('/destinations');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Youkoso!');
+            res.redirect('/destinations');
+        });
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
@@ -29,7 +31,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/destinations');
+    const redirectUrl = req.session.returnTo || '/destinations';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
