19caf455e822f1abfd42c823984f133e73c84bed
Vincent Nguyen <vdnguyen44@gmail.com>
2021-02-26 21:03:53 -0800

    testing edits during deploy

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'cluster-map',
-style: 'mapbox://styles/mapbox/dark-v10',
+style: 'mapbox://styles/mapbox/light-v10',
 center: [136.53085609203913, 38.83744843502025],
 zoom: 4
 });
